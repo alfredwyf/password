ae2123cebc83bab5fc7ba4f026cf7df20a12a260
Alfred Wong <wyfalfred@gmail.com>
2022-06-08 19:58:56 -0500

    password 3 times teacher enhanced version

--- password_teacher_version.py
+++ password_teacher_version.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 x = 3
-while True:
+while x > 0:
 	pwd = input('please input your password:')
 	if pwd == password:
 		print('you have sucessfully logged in')
@@ -8,5 +8,3 @@ while True:
 	else:
 		x = x - 1
 		print('please input a correct password, you have', x, 'chance left')
-		if x == 0:
-			break;
