0f3aa27cf102b07a924929ce82ac2c57e356d0c9
Alfred Wong <wyfalfred@gmail.com>
2022-06-10 17:35:33 -0500

    enhanced version

--- password_teacher_version.py
+++ password_teacher_version.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 x = 3
 while x > 0:
+	x = x - 1
 	pwd = input('please input your password:')
 	if pwd == password:
 		print('you have sucessfully logged in')
 		break;
 	else:
-		x = x - 1
-		print('please input a correct password, you have', x, 'chance left')
+		print('please input a correct password')
+		if x > 0:
+			print('you have', x, 'chance left')
+		else:
+			print('you have no chance left, your account is logged')
